1a06ada474636cf6e684c3b5c7d8e1d4151850ff
Sythiex <14610458+Sythiex@users.noreply.github.com>
2026-01-31 15:53:17 -0700

    Removed Battleship mod

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -294,23 +294,23 @@ upsert_technology({
 
 -- ---- Battleship requires ironclad, artillery, and rocket turrets ----
 
-upsert_technology({
-    type = "technology",
-    name = "battleship",
-    -- icon = "__cargo-ships-graphics__/graphics/technology/cargo_ships.png",
-    icon_size = 256,
-    effects = {unlock("battleship"), unlock("patrol-boat")},
-    prerequisites = {"cargo_ships", "ironclad", "artillery", "rocket-turret"},
-    unit = {
-        count = 2500,
-        ingredients = {
-            {"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"military-science-pack", 1}, {"chemical-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1},
-            {"metallurgic-science-pack", 1}, {"agricultural-science-pack", 1}
-        },
-        time = 30
-    },
-    order = "c-g-a"
-})
+-- upsert_technology({
+--     type = "technology",
+--     name = "battleship",
+--     -- icon = "__cargo-ships-graphics__/graphics/technology/cargo_ships.png",
+--     icon_size = 256,
+--     effects = {unlock("battleship"), unlock("patrol-boat")},
+--     prerequisites = {"cargo_ships", "ironclad", "artillery", "rocket-turret"},
+--     unit = {
+--         count = 2500,
+--         ingredients = {
+--             {"automation-science-pack", 1}, {"logistic-science-pack", 1}, {"military-science-pack", 1}, {"chemical-science-pack", 1}, {"utility-science-pack", 1}, {"space-science-pack", 1},
+--             {"metallurgic-science-pack", 1}, {"agricultural-science-pack", 1}
+--         },
+--         time = 30
+--     },
+--     order = "c-g-a"
+-- })
 
 -- ---- Hovercraft requires flying robot frames, missile hovercraft requires rocket turrets ----
 
