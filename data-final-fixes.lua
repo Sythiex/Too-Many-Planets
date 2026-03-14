b97566802f3ecf6838572c3c2042ba7c4521f76b
Sythiex <14610458+Sythiex@users.noreply.github.com>
2026-03-14 14:52:07 -0600

    v0.3.1

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,5 @@
 require("patches.aai-loader-electricity")
+require("patches.arig-tweaks")
 -- require("patches.battleship-integration") -- disabled due to battleship multiplayer incompatibility
 require("patches.hovercraft-integration")
 require("patches.kry-inserters-lignumis-fix")
---
+++ patches/arig-tweaks.lua
@@ -0,0 +1,5 @@
+local util = require("util")
+
+if mods["planetaris-arig"] and settings.startup["tmp-arig-buff-robots"].value == true then
+    data.raw.planet["arig"].surface_properties.pressure = 1200
+end
